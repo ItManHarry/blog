1c9520485b3b5073dec5a584d6ce81d168c3dfb1
itmanharry <jack_chengqian@163.com>
2021-05-06 15:17:44 +0800

    show post and post pagination

--- ddic/views/blog.py
+++ ddic/views/blog.py
@@ -1,14 +1,20 @@
-from flask import Blueprint, redirect,url_for,render_template,session,request,flash
+from flask import Blueprint, redirect,url_for,render_template,session,request,flash,current_app
 from ddic.models import Post
 from ddic.forms.all import PostForm
 from ddic.exts import db
 import uuid
 bp_blog = Blueprint('blog', __name__)
 #博客首页
-@bp_blog.route('/')
-def index():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', posts=posts)
+@bp_blog.route('/', defaults={'page':1})
+@bp_blog.route('/page/<int:page>')
+def index(page):
+    #page = request.args.get('page', 1, type=int)
+    #per_page = current_app.config['ITEM_COUNT_PER_PAGE']
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page, per_page=current_app.config['ITEM_COUNT_PER_PAGE'])
+    posts = pagination.items
+    #posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', posts=posts, pagination=pagination)
+#新增文章
 @bp_blog.route('/add', methods=['GET','POST'])
 def add():
     form = PostForm()
@@ -25,3 +31,8 @@ def add():
         flash('文章发布成功!!!')
         return redirect(url_for('.add'))
     return render_template('blog/add.html', form=form)
+#文章详情
+@bp_blog.route('/show/<post_id>')
+def show(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('blog/show.html', post=post)
